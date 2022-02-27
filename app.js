d4b32f40e1e2e57e3b2cac479ed7101455170a32
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-02-26 18:44:04 -0600

    clipboard api and bg gradient

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const color = document.getElementById('pick-color');
 let pickedColor = '';
 
@@ -8,6 +9,21 @@ const form = document.getElementById('get-color-scheme');
 const submitButton = document.getElementById('submit'); 
 
 let schemeData = '';
+let colorsArr = ['#F55A5A','#2B283A','#FBF3AB','#AAD1B6','#A626D3'];
+
+colorOne = document.querySelector('.color-one');
+colorTwo = document.querySelector('.color-two');
+colorThree = document.querySelector('.color-three');
+colorFour = document.querySelector('.color-four');
+colorFive = document.querySelector('.color-five');
+
+valueOne = document.querySelector('.value-one');
+valueTwo = document.querySelector('.value-two');
+valueThree = document.querySelector('.value-three');
+valueFour = document.querySelector('.value-four');
+valueFive = document.querySelector('.value-five');
+
+let colorDivsArr = document.querySelectorAll('.color');
 
 color.addEventListener('input', function() {
     pickedColor = this.value
@@ -19,39 +35,54 @@ scheme.addEventListener('input', function() {
     pickedScheme = pickedScheme.toLowerCase();
 })
 
+// getting data from The Color API
 form.addEventListener('submit', function(event) {
     event.preventDefault()
 
-    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor}&format=json&mode=${pickedScheme}&count=4`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor}&format=json&mode=${pickedScheme}&count=5`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         schemeData = data;
 
-        colorOne = document.querySelector('.color-one');
-        colorTwo = document.querySelector('.color-two');
-        colorThree = document.querySelector('.color-three');
-        colorFour = document.querySelector('.color-four');
-        colorFive = document.querySelector('.color-five');
+        let colorHexsArr = [ 
+            schemeData.colors[0].hex.value,
+            schemeData.colors[1].hex.value,
+            schemeData.colors[2].hex.value,
+            schemeData.colors[3].hex.value,
+            schemeData.colors[4].hex.value ];
 
-        colorOne.style.background = `#${pickedColor}`;
-        colorTwo.style.background = schemeData.colors[0].hex.value;
-        colorThree.style.background = schemeData.colors[1].hex.value;
-        colorFour.style.background = schemeData.colors[2].hex.value;
-        colorFive.style.background = schemeData.colors[3].hex.value;
+        body.style.background = `linear-gradient(to right, 
+            ${colorHexsArr[0]}, 
+            ${colorHexsArr[1]}, 
+            ${colorHexsArr[2]}, 
+            ${colorHexsArr[3]}, 
+            ${colorHexsArr[4]})`;
+    
+        colorOne.style.background = schemeData.colors[0].hex.value;
+        colorTwo.style.background = schemeData.colors[1].hex.value;
+        colorThree.style.background = schemeData.colors[2].hex.value;
+        colorFour.style.background = schemeData.colors[3].hex.value;
+        colorFive.style.background = schemeData.colors[4].hex.value;
 
-        valueOne = document.querySelector('.value-one');
-        valueTwo = document.querySelector('.value-two');
-        valueThree = document.querySelector('.value-three');
-        valueFour = document.querySelector('.value-four');
-        valueFive = document.querySelector('.value-five');
-
-        valueOne.innerText = `#${pickedColor}`;
-        valueTwo.innerText = schemeData.colors[0].hex.value;
-        valueThree.innerText = schemeData.colors[1].hex.value;
-        valueFour.innerText = schemeData.colors[2].hex.value;
-        valueFive.innerText = schemeData.colors[3].hex.value;
+        valueOne.innerText = schemeData.colors[0].hex.value;
+        valueTwo.innerText = schemeData.colors[1].hex.value;
+        valueThree.innerText = schemeData.colors[2].hex.value;
+        valueFour.innerText = schemeData.colors[3].hex.value;
+        valueFive.innerText = schemeData.colors[4].hex.value;
     })
 })
 
-/* <input type="color" oninput="alert(this.value)"></input> */
+// Copy to clipboard using Clipboard API
+for (let i=0; i<colorDivsArr.length; i++) {
+    colorDivsArr[i].addEventListener('click', function() {
+        
+        if (schemeData === '') {
+            navigator.clipboard.writeText(colorsArr[i]);
+            console.log(i, colorsArr[i])
+        } else {
+            let colorHex = schemeData.colors[i].hex.value;
+             navigator.clipboard.writeText(colorHex);
+             console.log(i, colorHex)
+        }
+    })
+}
